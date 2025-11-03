9aabcf787ca4c9315b759cc15bab659aa9c0c013
RajeshBasnet-dev <backendrex@gmail.com>
2025-11-03 21:19:37 +0545

    feat: add forms for customer, supplier, and business profile

---
+++ accounts/forms.py
@@ -0,0 +1,66 @@
+"""
+Forms for the accounts app in Digital Khata.
+"""
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Customer, Supplier, BusinessProfile
+
+class CustomUserCreationForm(UserCreationForm):
+    """Custom user creation form with email field."""
+    
+    email = forms.EmailField(required=True)
+    
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+    
+    def save(self, commit=True):
+        """Save the user with email."""
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+class CustomerForm(forms.ModelForm):
+    """Form for creating and editing customers."""
+    
+    class Meta:
+        model = Customer
+        fields = ['name', 'email', 'phone', 'address']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'address': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
+
+class SupplierForm(forms.ModelForm):
+    """Form for creating and editing suppliers."""
+    
+    class Meta:
+        model = Supplier
+        fields = ['name', 'email', 'phone', 'address']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'address': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
+
+class BusinessProfileForm(forms.ModelForm):
+    """Form for creating and editing business profiles."""
+    
+    class Meta:
+        model = BusinessProfile
+        fields = ['business_name', 'business_type', 'address', 'country', 'tax_id', 'currency']
+        widgets = {
+            'business_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'business_type': forms.Select(attrs={'class': 'form-select'}),
+            'address': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'country': forms.Select(attrs={'class': 'form-select'}),
+            'tax_id': forms.TextInput(attrs={'class': 'form-control'}),
+            'currency': forms.TextInput(attrs={'class': 'form-control'}),
+        }
